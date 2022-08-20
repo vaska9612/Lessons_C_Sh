5cd42b8d91c06d2f31cf89022ee12f8370dda19c
Екатерина Васильева <vaska96@yandex.ru>
2022-08-20 17:02:11 +0300

    Добавлена проверка корректности ИД

--- Homework_7/Program.cs
+++ Homework_7/Program.cs
@@ -8,32 +8,49 @@ namespace Homework_7
 {
     internal class Program
     {
+        // Программа сравнивает площади двух треугольников, используются Методы
         static void Main(string[] args)
         {
             Console.WriteLine("Введите длины сторон первого треугольника");
             int a1 = Convert.ToInt16(Console.ReadLine());
             int b1 = Convert.ToInt16(Console.ReadLine());
             int c1 = Convert.ToInt16(Console.ReadLine());
-            double S1 = 0;
-            Console.WriteLine("Введите длины сторон второго треугольника");
-            int a2 = Convert.ToInt16(Console.ReadLine());
-            int b2 = Convert.ToInt16(Console.ReadLine());
-            int c2 = Convert.ToInt16(Console.ReadLine());
-            double S2 = 0;
-            S1 = CalcArea(a1, b1, c1);
-            S2 = CalcArea(a2, b2, c2);
-            if (S1 > S2)
+            int a2, b2, c2;
+            a2 = b2 = c2 = 0;
+            bool Q = true;
+            if (((a1 + b1) <= c1 || (b1 + c1) <= a1 || (a1 + c1) <= b1))
             {
-                Console.WriteLine("Площадь первого треугольника больше");
+               Q = false;
+               Console.WriteLine("Такого треугольника не существует");
             }
             else
             {
-                if (S2 > S1)
-                    Console.WriteLine("Площадь второго треугольника больше");
+                Console.WriteLine("Введите длины сторон второго треугольника");
+                a2 = Convert.ToInt16(Console.ReadLine());
+                b2 = Convert.ToInt16(Console.ReadLine());
+                c2 = Convert.ToInt16(Console.ReadLine());
+                if (((a2 + b2) <= c2 || (b2 + c2) <= a2 || (a2 + c2) <= b2))
+                {
+                    Q = false;
+                    Console.WriteLine("Такого треугольника не существует");
+                }
+            }
+            if (Q == true)
+            {
+                double S1 = CalcArea(a1, b1, c1);
+                double S2 = CalcArea(a2, b2, c2);
+                if (S1 > S2)
+                {
+                    Console.WriteLine("Площадь первого треугольника больше");
+                }
                 else
-                    Console.WriteLine("Площади треугольников равны");
-            }           
-                Console.ReadKey();
+                {
+                    Console.WriteLine("Площадь второго треугольника больше");
+                }
+            }
+            else
+                Console.WriteLine("Ошибка в исходных данных. Программа не выполнена");
+            Console.ReadKey();
         }
         static double CalcArea(int a, int b, int c)
         {
@@ -41,7 +58,7 @@ namespace Homework_7
             double S = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
             Console.WriteLine(S);
             return S;
-            //нифига не работает!!!
         }
+
     }
 }
