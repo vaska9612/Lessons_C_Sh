e0fd9cf9e0d4d9be4cdec0e8b5968c9ebb311d0c
Екатерина Васильева <vaska96@yandex.ru>
2022-08-20 17:43:45 +0300

    Решила задачу№2 из блока 7.  V и S_пов куба

---
+++ Homework_7.1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_7._1
+{
+    // Программа вычисляет объем и площадь поверхности куба по длине его ребра
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину ребра куба");
+            int a = Convert.ToInt16(Console.ReadLine());
+            VolumeArea(a);
+            Console.ReadKey();
+        }
+        static void VolumeArea (int a)
+        {
+            int V = a * a * a;
+            int S = 6 * a * a;
+            Console.WriteLine("V = {0}, S_пов = {1}", V, S);
+        }
+    }
+}
