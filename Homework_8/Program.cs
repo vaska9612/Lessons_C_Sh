a1af6b7cfbbbf3420e5a7144d226fe22706c22a7
Екатерина Васильева <vaska96@yandex.ru>
2022-08-20 19:51:50 +0300

    Начала работать над блоком№8, но не закончила

---
+++ Homework_8/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Homework_8
+{
+    internal class Program
+    {
+        // Работа с файлами
+        //Выберите любую папку на своем компьютере, имеющую вложенные директории. Выведите на консоль ее содержимое и содержимое всех подкаталогов.
+        static void Main(string[] args)
+        {
+            string path = "D:/vaska/Desktop/C#/HW_8";
+            string[] directory = Directory.GetFiles(path);
+            foreach (string file in directory)
+            {
+                Console.WriteLine(file);
+                Directory.StreamReader 
+            }
+            Console.ReadKey();
+        }
+    }
+}
