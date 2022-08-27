a729a7aa3df594e9509023ed693d7e7e3bbad777
Екатерина Васильева <vaska96@yandex.ru>
2022-08-27 16:35:40 +0300

    Доработка Задачи№2 Блок8. Ошибка индекс вне границы массива

--- Homework_8.1/Program.cs
+++ Homework_8.1/Program.cs
@@ -13,20 +13,42 @@ namespace Homework_8._1
     {
         static void Main(string[] args)
         {
-            File.Create("D:/vaska/Desktop/C#/HW_8.1/numbers.txt").Close();
+            string file_path = "D:/vaska/Desktop/C#/HW_8.1/numbers.txt";
+            File.Create(file_path).Close();
             int[] numbers = new int[10];
             Random random = new Random();
             for (int i = 0; i < numbers.Length; i++)
             {
-                numbers[i] = random.Next();
-                using (StreamWriter sw = new StreamWriter("D:/vaska/Desktop/C#/HW_8.1/numbers.txt", true))
+                numbers[i] = random.Next(0, 100);
+                using (StreamWriter sw = new StreamWriter(file_path, true))
                 {
-                    sw.WriteLine("число N{0} = {1}", (i+1), numbers[i]);
+                    sw.WriteLine("число N{0} = {1}", (i + 1), numbers[i]);
                 }
-                
             }
+            StringBuilder sb = new StringBuilder();
+            using (StreamReader sr = new StreamReader(file_path))
+            {
+                string line;
+                int[] numbers_file = new int[10];
+                int i = 0;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    i++;
+                    int index = line.LastIndexOf(" ");
+                    line = line.Substring(index);
+                    numbers_file[i] = Convert.ToInt32(line);
+                }
+            }
+            Console.WriteLine(sb);
+            Console.ReadKey();            
 
 
         }
+        /*static int CulcSum(string file_path, out int Sum)
+        {
+
+        }*/
+
+
     }
 }
