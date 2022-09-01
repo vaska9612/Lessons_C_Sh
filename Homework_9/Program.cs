4a86647929a0f8d28bdfca9a8da3329202d7374c
Екатерина Васильева <vaska96@yandex.ru>
2022-09-01 21:13:22 +0300

    Сделала Задачу, Блок 9

---
+++ Homework_9/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое число");
+            int a = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Введите целое число");
+            int b = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Что с ними сделать?");
+            Console.WriteLine("1 - сложить\n2 - вычесть из первого второе\n3 - перемножить\n4 - поделить первое на второе");
+            int to_do = 0;
+            try
+            {
+                to_do = Convert.ToInt16(Console.ReadLine());
+                switch (to_do)
+                {
+                    case 1:
+                        {
+                            Console.WriteLine("Результат: {0}", a + b);
+                            break;
+                        }
+                    case 2:
+                        {
+                            Console.WriteLine("Результат: {0}", a - b);
+                            break;
+                        }
+                    case 3:
+                        {
+                            Console.WriteLine("Результат: {0}", a * b);
+                            break;
+                        }
+                    case 4:
+                        {
+                            int c = a / b; // Если бы этой строчки не было, при попытке делить на 0 выводится не сообщение об ошибке, а "Результат: ?"                            
+                            Console.WriteLine("Результат: {0}", (double) a / b);
+                            break;
+                        }
+                }
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Ошибка! Делить на ноль нельзя!");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Ошибка! Надо вводить именно целые числа!");
+            }
+            catch (Exception) when (to_do != 1 || to_do != 2 || to_do != 3 || to_do != 4)
+            {
+                Console.WriteLine("Ошибка! Нет функции с номером {0}", to_do);
+            }
+            Console.ReadKey();
+        }
+    }
+}
