a937448676ef5c6eb5ff4f7f65044d4aad41ce12
Екатерина Васильева <vaska96@yandex.ru>
2022-09-13 22:50:48 +0300

    Какие-то тут изменения есть, но это было неделю назад, я уже не помню ничего)

--- Homework_10/Program.cs
+++ Homework_10/Program.cs
@@ -22,11 +22,15 @@ namespace Homework_10
             Console.WriteLine("Введите целое число угловых секунд < 60");
             int sec = Convert.ToInt16(Console.ReadLine());
             Angle angle = new Angle(gradus, min, sec);
-            angle.ToRadians(gradus, min, sec, out _);
+            Console.WriteLine(angle.Gradus);
+            gradus = 36;
+            angle.ToChangeGradus(gradus);
+            Console.WriteLine(angle.Gradus);
             Console.ReadKey();
+            //int n = angle.ToRadians(gradus, min, sec);
         }
     }
-    class Angle
+    /*class Angle
     {
         public int Gradus
         {
@@ -88,12 +92,89 @@ namespace Homework_10
             Min = min;
             Sec = sec;
         }
-        public void ToRadians(int gradus, int min, int sec, out double radians)
+        public void ToRadians(int gradus, int min, int sec)
+        {
+           radians = (gradus + min / 60 + sec / 3600) / 180 * Math.PI;
+           Console.WriteLine(radians);
+        }
+    }*/
+
+    class Angle
+    {
+        private int gradus;
+        private int sec;
+        private int min;
+
+        public int Gradus
+        {            
+            set
+            {
+                if (value > 0 && value < 180)
+                {
+                    gradus = value;
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка! Ожидается целое число градусов, < 180");
+                }
+            }
+            get
+            {
+                return (gradus);
+            }
+        }
+        public int Min
+        {           
+            set
+            {
+                if (value > 0 && value < 60)
+                {
+                    min = value;
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка! Ожидается целое число угловых минут, < 60");
+                }
+            }
+            get
+            {
+                return (min);
+            }
+        }
+        public int Sec
+        {            
+            set
+            {
+                if (value > 0 && value <= 180)
+                {
+                    sec = value;
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка! Ожидается целое фисло угловых секунд, < 60");
+                }
+            }
+            get
+            {
+                return (sec);
+            }
+        }
+        public Angle(int gradus, int min, int sec)
+        {
+            Gradus = gradus;
+            Min = min;
+            Sec = sec;
+        }
+       /* public void ToRadians(int gradus, int min, int sec)
         {
             radians = (gradus + min / 60 + sec / 3600) / 180 * Math.PI;
             Console.WriteLine(radians);
+        }*/
+
+        public void ToChangeGradus(int gradus)
+        {
+            Gradus = gradus;
         }
     }
-
 
 }
