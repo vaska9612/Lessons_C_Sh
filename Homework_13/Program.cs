193bab1c01384bd3774cc6260be724c58fb562a2
Екатерина Васильева <vaska96@yandex.ru>
2022-09-20 22:51:37 +0300

    Классы Building вынесла из класса Program

--- Homework_13/Program.cs
+++ Homework_13/Program.cs
@@ -14,39 +14,37 @@ namespace Homework_13
             mult_building.Print();
             Console.ReadKey();
         }
-        class Building
+    }
+    class Building
+    {
+        public string Adress { get; set; }
+        public int Length { get; set; }
+        public int Width { get; set; }
+        public int Hight { get; set; }
+        public Building(string adress, int length, int width, int hight)
         {
-            public string Adress { get; set; }
-            public int Length { get; set; }
-            public int Width { get; set; }
-            public int Hight { get; set; }
-            public Building(string adress, int length, int width, int hight)
-            {
-                Adress = adress;
-                Length = length;
-                Width = width;
-                Hight = hight;
-            }
-            public void Print()
-            {
-                Console.WriteLine("Здание находится по адресу {0} /n Здание длиной {1}м, шириной {2}м и высотой {3}м", Adress, Length, Width, Hight);
-            }
+            Adress = adress;
+            Length = length;
+            Width = width;
+            Hight = hight;
         }
-        sealed class MuiltBuilding:Building
+        public void Print()
         {
-            public int Floors { get; set; }
-            public MuiltBuilding(string adress, int length, int width, int hight, int floors)
-                :base (adress, length, width, hight)
-            {
-                Floors = floors;
-            }
-            public void Print()
-            {
-                base.Print();
-                Console.WriteLine("Этажность: {0}", Floors);
-            }
+            Console.WriteLine("Здание находится по адресу {0} /n Здание длиной {1}м, шириной {2}м и высотой {3}м", Adress, Length, Width, Hight);
         }
-
-
+    }
+    sealed class MuiltBuilding : Building
+    {
+        public int Floors { get; set; }
+        public MuiltBuilding(string adress, int length, int width, int hight, int floors)
+            : base(adress, length, width, hight)
+        {
+            Floors = floors;
+        }
+        public void Print()
+        {
+            base.Print();
+            Console.WriteLine("Этажность: {0}", Floors);
+        }
     }
 }
